a778ebdd57512930a7f259e82e6660535492d3af
Márk Mihalik <mihalik.mark@gmail.com>
2024-04-07 16:27:59 +0200

    Review keymap

--- keymap.c
+++ keymap.c
@@ -30,8 +30,8 @@ enum layers {
     _WASD,
 };
 
-#define RAISE MO(_RAISE)
-#define LOWER MO(_LOWER)
+#define RAISE   MO(_RAISE)
+#define LOWER   MO(_LOWER)
 
 #define LAY_BAS TO(_QWERTZ)
 #define LAY_ASD TO(_WASD)
@@ -60,49 +60,49 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* LOWER
 * QWERTY
  * ,--------------------------------------------.                                           ,----------------------------------------------.
- * |   F1    |  F2  |  F3  |  F4  |   F5 |  F6  |                                           |  F7  |  F8  |  F9  |  F10 | F11  |  F12      |
+ * |   F12   |  F1  |  F2  |  F3  |  F4  |  F5  |                                           |  F6  |  F7  |  F8  |  F9  | F10  |  F11      |
  * |---------+------+------+------+------+------|                                           |------+------+------+------+------+-----------|
- * |   Tab   |   \  |   |  | None |   /  |   &  |                                           |   $  |   Ü  | None |   Ü  |   +  |    -      |
+ * |   Tab   | None | None | None | None | None |                                           | None |   Ü  | None |   Ü  |   ?  | CapsLock  |
  * |---------+------+------+------+------+------|                                           |------+------+------+------+------+-----------|
- * | LShift  | None |   $  |   #  |   =  |   '  |---------------.           ,---------------|   "  |   &  |   *  |   (  |   :  |    _      |
+ * | LShift  |   !  |   @  |   #  |   $  |   %  |---------------.           ,---------------|   ^  |   &  |   *  | None |   :  |  RShift   |
  * |---------+------+------+------+------+------| None |    [   |           |   ]   | None  |------+------+------+------+------+-----------|
- * | LCTRL   | None | None | None | None | None |------|--------|           |-------|-------|  -   |   _  |   +  |   {  |   /  |  RCTRL    |
+ * | LCTRL   | None | None | None | None | None |------|--------|           |-------|-------|   °  | None |   <  |   >  |   _  |  RCTRL    |
  * `--------------------------------------------|               /            \              |----------------------------------------------'
  *                         | LGUI | LALT | DEL  |LOWER | Space /              \Enter| RAISE |BackSP| RGUI | RALT |
  *                         `----------------------------------'                '---------------------------------'
  */
 [_LOWER] = LAYOUT(
-    KC_F12 , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                       KC_F6,   KC_F7,   KC_F8,   KC_F9 ,  KC_F10,  KC_F11,
-    _______, HU_BSLS, HU_PIPE, XXXXXXX, HU_SLSH, HU_AMPR,                                     XXXXXXX, HU_UDIA, XXXXXXX, HU_ODIA, HU_PLUS, HU_MINS,
-    _______, XXXXXXX, HU_DLR,  HU_HASH, HU_EQL,  HU_QUOT,                                     HU_DQUO, HU_AMPR, HU_ASTR, HU_LPRN, HU_COLN, HU_UNDS,
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, HU_LBRC, HU_RBRC, XXXXXXX, HU_MINS, HU_UNDS, HU_LABK, HU_RABK, HU_QUES, KC_RCTL,
+    KC_F12 , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     XXXXXXX, HU_UDIA, XXXXXXX, HU_ODIA, HU_QUES, _______,
+    _______, HU_EXLM, HU_AT,   HU_HASH, HU_DLR,  HU_PERC,                                     HU_CIRC, HU_AMPR, HU_ASTR, XXXXXXX, HU_COLN, _______,
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, HU_LBRC, HU_RBRC, XXXXXXX, HU_RNGA, XXXXXXX, HU_LABK, HU_RABK, HU_UNDS, _______,
                                _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 /* RAISE
  * ,--------------------------------------------.                                   ,----------------------------------------------.
- * |   None  |  !   |  @   |  #   |   $  |   %  |                                   |  °   |   &  |   *  |   (  |   )  |    None   |
+ * |   None  |  !   |  @   |  #   |   $  |   %  |                                   |  ^   |   &  |   *  |   (  |   )  |     ~     |
  * |---------+------+------+------+------+------|                                   |------+------+------+------+------+-----------|
- * |   None  |   \  |   |  |  É   |   /  | None |                                   | Home |   Ú  |   Í  |   Ó  | None |    None   |
+ * |   None  |   \  |   |  |  É   |   /  |  ~   |                                   | Home |   Ú  |   Í  |   Ó  | None |    None   |
  * |---------+------+------+------+------+------|                                   |------+------+------+------+------+-----------|
- * |  LShift |   Á  | None | None | None | None |---------------.   ,---------------| End  | Left | Down |  Up  |Right |  RShift   |
+ * |  LShift |   Á  |   '  |   "  |   (  |   )  |---------------.   ,---------------| End  | Left | Down |  Up  |Right |  RShift   |
  * |---------+------+------+------+------+------| None |   {    |   |   }   | None  |------+------+------+------+------+-----------|
- * |  LCTRL  | None | None | None | None | None |------|--------|   |-------|-------|   +  |   -  |   =  |   _  | None |  RCTRL    |
+ * |  LCTRL  | None | None | None |   [  |   ]  |------|--------|   |-------|-------|   {  |   }  |   +  |   -  |   =  |  RCTRL    |
  * `--------------------------------------------|               /    \              |----------------------------------------------'
  *                         | LCTRL| LGUI |LALT  |LOWER | Space /      \Enter| RAISE |BackSP| RGUI | RALT |
  *                         `----------------------------------'        '---------------------------------'
  */
  [_RAISE] = LAYOUT(
     XXXXXXX, HU_EXLM, HU_AT,   HU_HASH, HU_DLR,  HU_PERC,                                       HU_CIRC, HU_AMPR, HU_ASTR, HU_LPRN, HU_RPRN, HU_TILD,
-    XXXXXXX, HU_BSLS, HU_PIPE, HU_EACU, HU_SLSH, XXXXXXX,                                       KC_HOME, HU_UACU, HU_IACU, HU_OACU, XXXXXXX, XXXXXXX,
-    _______, HU_AACU, HU_EQL,  HU_HASH, XXXXXXX, XXXXXXX,                                       KC_END, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,_______,
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, HU_LCBR,  HU_RCBR, XXXXXXX, HU_PLUS, HU_MINS, HU_EQL, HU_UNDS, XXXXXXX, _______,
+    XXXXXXX, HU_BSLS, HU_PIPE, HU_EACU, HU_SLSH, HU_TILD,                                       KC_HOME, HU_UACU, HU_IACU, HU_OACU, XXXXXXX, XXXXXXX,
+    _______, HU_AACU, HU_QUOT, HU_DQUO, HU_LPRN, HU_RPRN,                                       KC_END,  KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,_______,
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, HU_LBRC, HU_RBRC,  XXXXXXX, HU_LCBR,  HU_RCBR, XXXXXXX, HU_LCBR, HU_RCBR, HU_PLUS, HU_MINS, HU_EQL,  _______,
                                _______, _______, _______,  _______, _______,  _______, _______,  _______, _______, _______
 ),
 
 /* ADJUST (Press LOWER and RAISE together)
  * ,--------------------------------------------.                                   ,----------------------------------------------.
- * | QK_BOOT |      |      |      |      | None |                                   |      |      |      |      |      |   WASDq    |
+ * | QK_BOOT |      |      |      |      | WASD  |                                   |      |      |      |      |      |           |
  * |---------+------+------+------+------+------|                                   |------+------+------+------+------+-----------|
  * | RGB ON  | HUE+ | SAT+ | VAL+ |      |      |                                   | None |  Ű   | Play |   Ő  |      |           |
  * |---------+------+------+------+------+------|                                   |------+------+------+------+------+-----------|
@@ -114,7 +114,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                         `----------------------------------'        '---------------------------------'
  */
 [_ADJUST] = LAYOUT(
-    QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, LAY_ASD,
+    QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, LAY_ASD,                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, KC_BRIU, XXXXXXX,                                     XXXXXXX, HU_UDAC, KC_MPLY, HU_ODAC, XXXXXXX, XXXXXXX,
     RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, KC_BRID, XXXXXXX,                                     XXXXXXX, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, XXXXXXX,
     XXXXXXX, XXXXXXX, KC_WAKE, KC_SLEP, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MRWD, KC_MUTE, KC_MSTP, KC_MFFD, XXXXXXX,
@@ -123,26 +123,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* WASD
  * ,--------------------------------------------.                                   ,----------------------------------------------.
- * |   ESC   |   1  |   2  |   3  |   4  |   5  |                                   | None | None | None | None | None |    BASE   |
+ * |   None  | None | None | None | None | BASE |                                   | None | None | None | None | None |           |
  * |---------+------+------+------+------+------|                                   |------+------+------+------+------+-----------|
- * |   Tab   |   1  |   Q  |   W  |   E  |  R   |                                   | None | None | None | None | None |   None    |
+ * |   None  | None |   Q  |   W  |   E  | None |                                   | None | None | None | None | None |           |
  * |---------+------+------+------+------+------|                                   |------+------+------+------+------+-----------|
- * |  LShift |LShift|   A  |  S  |   D   |  F   |---------------.   ,---------------| Left | Down |  Up  |Right | None |   None    |
- * |---------+------+------+------+------+------| None |   G    |   |   }   | None  |------+------+------+------+------+-----------|
- * |  LCNTRL |   Y  |   X  |  B  |  C   |  V   |------|--------|   |-------|-------|  None | None | None | None | None |   None    |
+ * |  LShift |LShift|   A  |  S  |   D   | None |---------------.   ,---------------| Left | Down |  Up  |Right |   ;  |           |
+ * |---------+------+------+------+------+------| None |   {    |   |   }   | None  |------+------+------+------+------+-----------|
+ * |  LCNTRL | None | None | None | None |  C   |------|--------|   |-------|-------|   +  |   -  |   =  |  [  |   ]   |   \       |
  * `--------------------------------------------|               /    \              |----------------------------------------------'
- *                         |  None| None |  M   | None | Space /      \Enter| RAISE |BackSP| RGUI | RALT |
+ *                         |  None| None | None | Space|  None /      \Enter| RAISE |BackSP| RGUI | RALT |
  *                         `----------------------------------'        '---------------------------------'
  */
 [_WASD] = LAYOUT(
-    KC_ESC,  HU_1,    HU_2,    HU_3,    HU_4,    HU_5,                                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, LAY_BAS,
-    KC_TAB,  HU_1,    HU_Q,    HU_W,    HU_E,    HU_R,                                        XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, XXXXXXX,
-    KC_LSFT, KC_LSFT, HU_A,    HU_S,    HU_D,    HU_F,                                        XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,
-    KC_LCTL, HU_Y,    HU_X,    HU_B,    HU_C,    HU_V,    XXXXXXX, HU_G,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                               XXXXXXX, XXXXXXX, HU_M,    XXXXXXX, KC_SPC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, LAY_BAS,                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, HU_Q,    HU_W,    HU_E,    XXXXXXX,                                     XXXXXXX, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, XXXXXXX,
+    KC_LSFT, KC_LSFT, HU_A,    HU_S,    HU_D,    XXXXXXX,                                     XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,
+    KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, HU_C,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                               XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX
 )
 
 };
+
+
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     state = update_tri_layer_state(state, _RAISE, _LOWER, _ADJUST);
